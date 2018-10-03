81432774aeca9d0e3e1ceb12c0aa96d918f4ddba
Paulo Sérgio <39868740+PauloSanches12@users.noreply.github.com>
2018-10-02 23:30:43 -0300

    Add files via upload

---
+++ mp_orcamento/mp_orcamento/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('orcamentos/', orcamentos_lista, name='orcamentos-lista'),
+    path('orcamentos/estatisticas/', orcamentos_estatisticas,name='orcamentos-estatisticas'),
+    path('orcamentos/cliente/<int:id_cliente>/', info_cliente,name='Informacao-cliente'),
+    path('orcamentos/cliente/estatistica/', estatistica,name='Estatistica'),
+]
